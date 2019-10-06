2763647fe6c3fbe1e06ba4177e2f6c17b33c9f9b
sandeep0383 <sandeep.tester83@gmail.com>
2019-10-06 02:48:20 +0100

    Argos web application automation

---
+++ src/main/java/com/argos999/qa/App.java
@@ -0,0 +1,13 @@
+package com.argos999.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
